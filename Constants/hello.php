4c8f492f0185ca83d1090be9bb683704a73e2745
rishabhdeogade07 <158757723+rizzab@users.noreply.github.com>
2025-07-27 18:26:35 +0530

    Create hello.php

---
+++ Constants/hello.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name="Daniel";
+    $name="John";
+    echo $name;
+    echo "<br>";
+    define("PI",3.14);
+    echo PI;
+    echo "<br>";
+    define("NAME","Rishabh");
+    echo NAME;
+    echo "<br>";
+    function greet() {
+        echo PI;
+    }
+    greet();
+    ?>
+</body>
+</html>
