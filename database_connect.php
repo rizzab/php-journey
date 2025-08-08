66b0d537106357ed466d36e18ba8a66c7fcb1078
rishabhdeogade07 <158757723+rizzab@users.noreply.github.com>
2025-08-08 13:26:55 +0530

    Create database_connect.php

---
+++ database_connect.php
@@ -0,0 +1,15 @@
+<?php
+$dsn='mysql:host=localhost;dbname=myfirstdatabase';
+$dbusername='root';
+$dbpassword='root';
+
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}catch(){
+
+}
+?>
